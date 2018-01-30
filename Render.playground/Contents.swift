5672b83963cc2e21b64001ecd46691a3387cd6e5
Florian  Kraupa <fkraupa@yahoo.com>
2018-01-30 14:11:00 +0100

    Added Explainations
    Ev

--- Render.playground/Contents.swift
+++ Render.playground/Contents.swift
@@ -3,6 +3,7 @@ extension String: explicit {}
 // this one is universal for every branch
 public func make(input: Array<position>) {
     var mat = matrix<String>()
+    //5 rows: the central line + 2x conecting lines + 2x H atoms
     mat.rows = 5
         //these are only for the main branch 
     mat.colums = (input.count / 2) - 1
